8fb9344244a9df0d273b74ba260315e1c70bd22c
mars <maspam@yandex.ru>
2020-10-12 00:53:56 +0300

    добавил `состояние` в компонент написания отзыва и вынес рейтинг (элемент звездочек) в отд компонент

--- src/components/add-review/add-review.jsx
+++ src/components/add-review/add-review.jsx
@@ -1,82 +1,135 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
+import RatingStars from '../rating-stars/rating-stars';
+import {generateId} from '../../utils.js';
 
 
-const AddReview = () => {
+class AddReview extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ratingStarsChecks: [false, false, true, false, false],
+      text: ``,
+    };
 
-  return (
-    <section className="movie-card movie-card--full">
-      <div className="movie-card__header">
-        <div className="movie-card__bg">
-          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+  }
+
+  handleStarClick(checked, index) {
+    const newRatingStarsCheked = Array(5).fill(false);
+    newRatingStarsCheked[index] = checked;
+    this.setState({
+      ratingStarsChecks: newRatingStarsCheked,
+    });
+  }
+
+  handleFormSubmit() {}
+
+  handleTextChange(text) {
+    this.setState({
+      text,
+    });
+  }
+
+  render() {
+    const ratingStarsChecks = this.state.ratingStarsChecks;
+    const id = Number(this.props.match.params.id);
+    const currentMovie = this.props.movies.find((movie) => movie.id === id);
+    const {title, background, poster} = currentMovie;
+
+    return (
+      <section className="movie-card movie-card--full">
+        <div className="movie-card__header">
+          <div className="movie-card__bg">
+            <img src={background} alt={title} />
+          </div>
+
+          <h1 className="visually-hidden">WTW</h1>
+
+          <header className="page-header">
+            <div className="logo">
+              <Link to="/" className="logo__link">
+                <span className="logo__letter logo__letter--1">W</span>
+                <span className="logo__letter logo__letter--2">T</span>
+                <span className="logo__letter logo__letter--3">W</span>
+              </Link>
+            </div>
+
+            <nav className="breadcrumbs">
+              <ul className="breadcrumbs__list">
+                <li className="breadcrumbs__item">
+                  <Link to={`/films/${id}`} className="breadcrumbs__link">{title}</Link>
+                </li>
+                <li className="breadcrumbs__item">
+                  <a className="breadcrumbs__link">Add review</a>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="user-block">
+              <div className="user-block__avatar">
+                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+              </div>
+            </div>
+          </header>
+
+          <div className="movie-card__poster movie-card__poster--small">
+            <img src={poster} alt={title} width="218" height="327" />
+          </div>
         </div>
 
-        <h1 className="visually-hidden">WTW</h1>
-
-        <header className="page-header">
-          <div className="logo">
-            <a href="main.html" className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
-
-          <nav className="breadcrumbs">
-            <ul className="breadcrumbs__list">
-              <li className="breadcrumbs__item">
-                <a href="movie-page.html" className="breadcrumbs__link">The Grand Budapest Hotel</a>
-              </li>
-              <li className="breadcrumbs__item">
-                <a className="breadcrumbs__link">Add review</a>
-              </li>
-            </ul>
-          </nav>
-
-          <div className="user-block">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+        <div className="add-review">
+          <form action="#" className="add-review__form" onSubmit={() => this.handleFormSubmit()}>
+            <div className="rating">
+              <div className="rating__stars">
+                {ratingStarsChecks.map((starCheck, index) => (
+                  <RatingStars
+                    key={generateId()}
+                    index={index}
+                    starCheck={starCheck}
+                    onStarClick={(checked) => this.handleStarClick(checked, index)} />
+                ))}
+              </div>
             </div>
-          </div>
-        </header>
 
-        <div className="movie-card__poster movie-card__poster--small">
-          <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
+            <div className="add-review__text">
+              <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"
+                onChange={(event) => this.handleTextChange(event.target.value)} >
+              </textarea>
+              <div className="add-review__submit">
+                <button className="add-review__btn" type="submit">Post</button>
+              </div>
+
+            </div>
+          </form>
         </div>
-      </div>
 
-      <div className="add-review">
-        <form action="#" className="add-review__form">
-          <div className="rating">
-            <div className="rating__stars">
-              <input className="rating__input" id="star-1" type="radio" name="rating" value="1"/>
-              <label className="rating__label" htmlFor="star-1">Rating 1</label>
+      </section>
+    );
+  }
+}
 
-              <input className="rating__input" id="star-2" type="radio" name="rating" value="2" />
-              <label className="rating__label" htmlFor="star-2">Rating 2</label>
-
-              <input className="rating__input" id="star-3" type="radio" name="rating" value="3" checked />
-              <label className="rating__label" htmlFor="star-3">Rating 3</label>
-
-              <input className="rating__input" id="star-4" type="radio" name="rating" value="4" />
-              <label className="rating__label" htmlFor="star-4">Rating 4</label>
-
-              <input className="rating__input" id="star-5" type="radio" name="rating" value="5" />
-              <label className="rating__label" htmlFor="star-5">Rating 5</label>
-            </div>
-          </div>
-
-          <div className="add-review__text">
-            <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text"></textarea>
-            <div className="add-review__submit">
-              <button className="add-review__btn" type="submit">Post</button>
-            </div>
-
-          </div>
-        </form>
-      </div>
-
-    </section>
-  );
+AddReview.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    preview: PropTypes.string.isRequired,
+    previewVideo: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    background: PropTypes.string.isRequired,
+    releaseYear: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    ratingText: PropTypes.string.isRequired,
+    votes: PropTypes.number.isRequired,
+    director: PropTypes.string.isRequired,
+    starringShort: PropTypes.string.isRequired,
+    starring: PropTypes.string.isRequired,
+    runtime: PropTypes.string.isRequired,
+    myList: PropTypes.bool.isRequired,
+  })).isRequired,
+  match: PropTypes.object.isRequired,
 };
 
 
