15fff428f509222ef0d545eedcaca63a2cedaa86
mars <maspam@yandex.ru>
2020-11-12 05:52:28 +0300

    перенес проверку загрузки данных с сервера на уровень выше в app устранил проблему с тем, что не открывались страницы напрямую через адресную строку

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,66 +1,47 @@
 import React from 'react';
-import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import GenreList from 'components/genre-list/genre-list';
 import Promo from 'components/promo/promo';
 import MovieListContainer from 'components/movie-list-container/movie-list-container';
-import {init} from 'store/api-action';
 
 
-class Main extends React.PureComponent {
-  componentDidMount() {
-    const {initAction} = this.props;
-    initAction();
-  }
+const Main = (props) => {
+  const {onMovieCardClick} = props;
 
-  render() {
-    const {onMovieCardClick, isApplicationReady} = this.props;
+  return (
+    <React.Fragment>
 
-    return (
-      <React.Fragment>
+      <Promo />
 
-        {isApplicationReady && <Promo />}
+      <div className="page-content">
+        <section className="catalog">
+          <h2 className="catalog__title visually-hidden">Catalog</h2>
 
-        <div className="page-content">
-          <section className="catalog">
-            <h2 className="catalog__title visually-hidden">Catalog</h2>
+          <GenreList />
+          <MovieListContainer onMovieCardClick={onMovieCardClick}/>
 
-            {isApplicationReady && <GenreList />}
-            {isApplicationReady && <MovieListContainer onMovieCardClick={onMovieCardClick}/>}
+        </section>
+        <footer className="page-footer">
+          <div className="logo">
+            <a className="logo__link logo__link--light">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
 
-          </section>
-          <footer className="page-footer">
-            <div className="logo">
-              <a className="logo__link logo__link--light">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </a>
-            </div>
-
-            <div className="copyright">
-              <p>© 2020 What to watch Ltd.</p>
-            </div>
-          </footer>
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+          <div className="copyright">
+            <p>© 2020 What to watch Ltd.</p>
+          </div>
+        </footer>
+      </div>
+    </React.Fragment>
+  );
+};
 
 Main.propTypes = {
   onMovieCardClick: PropTypes.func.isRequired,
-  isApplicationReady: PropTypes.bool.isRequired,
-  initAction: PropTypes.func.isRequired,
-};
-
-const mapStateToProps = ({moviesState}) => ({
-  isApplicationReady: moviesState.isApplicationReady,
-});
-
-const mapDispatchToProps = {
-  initAction: init,
 };
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+export default Main;
---
+++ src/components/routing/routing.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Main from 'components/main/main';
+import SignIn from 'components/sign-in/sign-in';
+import MyList from 'components/my-list/my-list';
+import Movie from 'components/movie/movie';
+import Review from 'components/review/review';
+import Player from 'components/player/player';
+
+
+const Routing = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'
+          render={({history}) => (
+            <Main onMovieCardClick={(id) => history.push(`/films/${id}`)} />
+          )}
+        />
+        <Route exact path='/login'>
+          <SignIn />
+        </Route>
+        <Route exact path='/mylist'
+          render={({history}) => (
+            <MyList onMovieCardClick={(id) => history.push(`/films/${id}`)} />
+          )}
+        />
+        <Route exact path='/films/:id'
+          render={({history, match}) => (
+            <Movie match={match}
+              onMovieCardClick={(id) => history.push(`/films/${id}`)} />
+          )}
+        />
+        <Route exact path='/films/:id/review'
+          render={({match}) => (
+            <Review match={match}/>
+          )}
+        />
+        <Route exact path='/player/:id'>
+          <Player />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+
+export default Routing;
