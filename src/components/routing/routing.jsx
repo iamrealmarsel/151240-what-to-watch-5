34fb2a973809f67c999ac5f8f52dbc56fb077c8a
mars <maspam@yandex.ru>
2020-11-14 18:07:34 +0300

    добавил приватные маршруты

--- src/components/routing/routing.jsx
+++ src/components/routing/routing.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {Router, Switch, Route, Redirect} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import browserHistory from 'browser-history';
 import Main from 'components/main/main';
 import SignIn from 'components/sign-in/sign-in';
 import MyList from 'components/my-list/my-list';
 import Movie from 'components/movie/movie';
 import Review from 'components/review/review';
 import Player from 'components/player/player';
+import PrivateRouting from 'components/private-routing/private-routing';
 
 
-const Routing = () => {
+const Routing = (props) => {
   return (
-    <BrowserRouter>
+    <Router history={browserHistory}>
       <Switch>
         <Route exact path='/'
           render={({history}) => (
@@ -18,20 +22,21 @@ const Routing = () => {
           )}
         />
         <Route exact path='/login'>
-          <SignIn />
+          { props.isAuthenticated
+            ? <Redirect to='/' />
+            : <SignIn />
+          }
         </Route>
-        <Route exact path='/mylist'
-          render={({history}) => (
-            <MyList onMovieCardClick={(id) => history.push(`/films/${id}`)} />
-          )}
-        />
+        <PrivateRouting exact path='/mylist' render={({history}) => (
+          <MyList onMovieCardClick={(id) => history.push(`/films/${id}`)} />
+        )} />
         <Route exact path='/films/:id'
           render={({history, match}) => (
             <Movie match={match}
               onMovieCardClick={(id) => history.push(`/films/${id}`)} />
           )}
         />
-        <Route exact path='/films/:id/review'
+        <PrivateRouting exact path='/films/:id/review'
           render={({match}) => (
             <Review match={match}/>
           )}
@@ -40,9 +45,17 @@ const Routing = () => {
           <Player />
         </Route>
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 };
 
+Routing.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+};
 
-export default Routing;
+const mapStateToProps = ({user}) => ({
+  isAuthenticated: user.isAuthenticated,
+});
+
+
+export default connect(mapStateToProps)(Routing);
